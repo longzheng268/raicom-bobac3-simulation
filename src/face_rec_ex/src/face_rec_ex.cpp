12962dba6d3dceb05f78b8ae2d9af45ae0cd7cfc
longzheng268 <longzheng0315@foxmail.com>
2026-05-25 14:18:37 +0800

    fix: face_rec_ex 修复段错误并改为动态播报识别到的人脸
    - 添加 face_num > 0 检查，防止空 vector 越界访问导致 SIGSEGV
    - 移除硬编码姓名，改为循环播报所有检测到的人脸名称
    - 清理未使用的 action client 指针和 goto_nav 声明
    
    Co-Authored-By: XiaomiMiMo <XiaomiMiMo@users.noreply.github.com>

--- src/face_rec_ex/src/face_rec_ex.cpp
+++ src/face_rec_ex/src/face_rec_ex.cpp
@@ -1,6 +1,5 @@
-#include<ros/ros.h>
-#include<face_rec/recognition_results.h>
 #include <ros/ros.h>
+#include <face_rec/recognition_results.h>
 #include <robot_audio/robot_iat.h>
 #include <robot_audio/Collect.h>
 #include <robot_audio/robot_tts.h>
@@ -10,27 +9,22 @@
 #include <string>
 using namespace std;
 
-typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> AC;//定义用到的action客户端的别名
-
 class interaction{
     public:
         interaction();
-        string voice_collect(); //语音采集
-        string voice_dictation(const char* filename); //语音听写
-        string voice_tts(const char* text); //语音合成
-        void goto_nav(struct Point* point); //导航到目标位置
+        string voice_collect();
+        string voice_dictation(const char* filename);
+        string voice_tts(const char* text);
     private:
-        ros::NodeHandle n; //创建一个节点句柄
-        actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>* ac; //创建action客户端对象指针
-        ros::ServiceClient collect_client,dictation_client,tts_client; //创建客户端
+        ros::NodeHandle n;
+        ros::ServiceClient collect_client,dictation_client,tts_client;
 };
 interaction::interaction(){
-    collect_client = n.serviceClient<robot_audio::Collect>("voice_collect"); //定义语音采集客户端
-    dictation_client = n.serviceClient<robot_audio::robot_iat>("voice_iat"); //定义语音听写客户端
-    tts_client = n.serviceClient<robot_audio::robot_tts>("voice_tts"); //定义语音合成客户端
+    collect_client = n.serviceClient<robot_audio::Collect>("voice_collect");
+    dictation_client = n.serviceClient<robot_audio::robot_iat>("voice_iat");
+    tts_client = n.serviceClient<robot_audio::robot_tts>("voice_tts");
 }
 string interaction::voice_collect(){
-    //请求"voice_collect"服务，返回音频保存位置
     ros::service::waitForService("voice_collect");
     robot_audio::Collect srv;
     srv.request.collect_flag = 1;
@@ -38,7 +32,6 @@ string interaction::voice_collect(){
     return srv.response.voice_filename;
 }
 string interaction::voice_dictation(const char* filename){
-    //请求"voice_dictation"服务，返回听写出的文本
     ros::service::waitForService("voice_iat");
     robot_audio::robot_iat srv;
     srv.request.audiopath = filename;
@@ -46,7 +39,6 @@ string interaction::voice_dictation(const char* filename){
     return srv.response.text;
 }
 string interaction::voice_tts(const char* text){
-    //请求"voice_tts"服务，返回合成的文件目录
     ros::service::waitForService("voice_tts");
     robot_audio::robot_tts srv;
     srv.request.text = text;
@@ -57,45 +49,55 @@ string interaction::voice_tts(const char* text){
     return srv.response.audiopath;
 }
 
-
-
 int main(int argc,char** argv)
 {
-   
-  ros::init(argc,argv,"face_rec_ex");
-  ros::NodeHandle n;
-  ros::ServiceClient face_rec_client = n.serviceClient<face_rec::recognition_results>("face_recognition_results");
-  face_rec::recognition_results face_rec_data;
-  ros::service::waitForService("face_recognition_results");
-  interaction audio; //创建一个交互实例
-  string dir,text,path; //创建两个字符串变量
-  while(ros::ok())
-  {
-     face_rec_data.request.mode = 1;
-     face_rec_client.call(face_rec_data);
-     if(face_rec_data.response.success)
-        {
-           int face_num = face_rec_data.response.result.num;
-           cout<<"我看到了"<<face_num<<"个人脸,他们分别是："<<endl;
-           for(int i=0;i<face_num;i++)
-           {
-            string rec_name = face_rec_data.response.result.face_data[i].name;
-            cout<<rec_name<<" ";
-           }
-	   dir = audio.voice_collect(); //采集语音
-           text = audio.voice_dictation(dir.c_str()).c_str(); //语音听写
-           if(text.find("我是谁") != string::npos)
-		{
-			audio.voice_tts("好好好的，让我看看"); //合成应答语音
-			if(face_rec_data.response.result.face_data[0].name=="龙正")
-			{
-				audio.voice_tts("龙正"); //合成应答语音
-			}
-			
-		}
-           cout<<endl;
-        }
-  }
-  return true;
-}
+    ros::init(argc,argv,"face_rec_ex");
+    ros::NodeHandle n;
+    ros::ServiceClient face_rec_client = n.serviceClient<face_rec::recognition_results>("face_recognition_results");
+    face_rec::recognition_results face_rec_data;
+    ros::service::waitForService("face_recognition_results");
+    interaction audio;
+    string dir, text;
 
+    while(ros::ok())
+    {
+        face_rec_data.request.mode = 1;
+        face_rec_client.call(face_rec_data);
+
+        if(face_rec_data.response.success)
+        {
+            int face_num = face_rec_data.response.result.num;
+            cout << "检测到 " << face_num << " 个人脸" << endl;
+
+            if(face_num > 0)
+            {
+                // 语音播报所有人脸名称
+                string name_list = "我看到了";
+                for(int i = 0; i < face_num; i++)
+                {
+                    string name = face_rec_data.response.result.face_data[i].name;
+                    cout << name << " ";
+                    if(i > 0) name_list += "和";
+                    name_list += name;
+                }
+                cout << endl;
+
+                // 采集语音并听写
+                dir = audio.voice_collect();
+                text = audio.voice_dictation(dir.c_str());
+
+                // 回答"我是谁"
+                if(text.find("我是谁") != string::npos)
+                {
+                    audio.voice_tts("好好好的，让我看看");
+                    for(int i = 0; i < face_num; i++)
+                    {
+                        string name = face_rec_data.response.result.face_data[i].name;
+                        audio.voice_tts(name.c_str());
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
